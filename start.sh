417ae92291d6ce491625a73abd7fc0f5902f10a3
Amovierequest <119895989+Amovierequest@users.noreply.github.com>
2022-12-25 12:38:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,8 @@
-echo "Cloning Repo...."
-
-git clone https://github.com/Amovierequest/MdiskSearchBot2.0.git /MdiskSearchBot2.0
-
+echo "Cloning Repo...."g
+it clone https://github.com/Amovierequest/MdiskSearchBot2.0.git /MdiskSearchBot2.0
 pip3 install -r requirements.txt
-
 cd /MdiskSearchBot2.0
-
 pip3 install -r requirements.txt
-
 echo "Starting Bot...."
-
 python3 main.py
-
 Footer
